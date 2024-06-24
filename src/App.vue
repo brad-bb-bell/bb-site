ba80cce7f0b93485ce7db0e45ea973231dbae4a4
Brad Bell <brad.bb.bell@gmail.com>
2024-06-24 10:22:58 -0600

    nav underlines

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,26 @@
       <h1 class="text-center text-6xl">Brad Bell</h1>
       <nav class="w-4/5 mx-auto mt-4">
         <ul class="flex flex-row justify-between">
-          <li><router-link to="/">Me</router-link></li>
-          <li><router-link to="/words">Words</router-link></li>
-          <li><router-link to="/learn">Learn</router-link></li>
-          <li><router-link to="/contact">Contact</router-link></li>
+          <li>
+            <router-link to="/" exact-active-class="underline" class="hover:underline"
+              >Me</router-link
+            >
+          </li>
+          <li>
+            <router-link to="/words" active-class="underline" class="hover:underline"
+              >Words</router-link
+            >
+          </li>
+          <li>
+            <router-link to="/learn" active-class="underline" class="hover:underline"
+              >Learn</router-link
+            >
+          </li>
+          <li>
+            <router-link to="/contact" active-class="underline" class="hover:underline"
+              >Contact</router-link
+            >
+          </li>
         </ul>
       </nav>
       <router-view />
