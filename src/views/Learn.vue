6dc52c2f644e1958445e0ce25d18370290aea86b
Brad Bell <brad.bb.bell@gmail.com>
2024-06-26 08:16:20 -0600

    updates learn

--- src/views/Learn.vue
+++ src/views/Learn.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <h1>Learn</h1>
-    <p>Learn go here</p>
-  </div>
+  <main class="text-center mt-6 mx-6 text-purple-50">
+    <p>Each month I post about something I learned in regards to software development.</p>
+  </main>
 </template>
