c42697569c290e2da46a7967a522a17d6ba321ec
Brad Bell <brad.bb.bell@gmail.com>
2024-06-26 08:35:18 -0600

    updates contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <h1>Contact</h1>
-    <p>Contact go here</p>
-  </div>
+  <main class="text-center mt-6 mx-6 text-purple-50 leading-8">
+    <p>brad.bb.bell@gmail.com</p>
+    <p><a href="https://www.linkedin.com/in/brad-bb-bell/">linkedin.com/in/brad-bb-bell</a></p>
+    <p><a href="https://github.com/brad-bb-bell">github.com/brad-bb-bell</a></p>
+    <p><a href="https://www.instagram.com/ballinbu/">instagram.com/ballinbu/</a></p>
+    <p><a href="https://x.com/dadmodebb">x.com/dadmodebb</a></p>
+  </main>
 </template>
